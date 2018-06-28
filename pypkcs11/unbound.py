21cd61c3b6933fbc80aad879b60d450b1ed19af0
Michael Kraitsberg <michael.kraitsberg@dyadicsec.com>
2018-06-28 15:29:18 +0300

    DYC_SignX509

--- pypkcs11/unbound.py
+++ pypkcs11/unbound.py
@@ -2,14 +2,15 @@
     Interface to the following Unbound vendor-specific functions:
 
 * DYC_SelfSignX509
+* DYC_SignX509
 """
-from ctypes import cast, string_at, c_ubyte
+from ctypes import cast, string_at, c_ubyte, create_string_buffer
 from _ctypes import POINTER
 
 from .conversions import from_bytestring
 from .attributes import to_byte_array
 from .common_utils import refresh_c_arrays, AutoCArray
-from .cryptoki import CK_ULONG, DYC_SelfSignX509, CK_MECHANISM_TYPE
+from .cryptoki import CK_ULONG, CK_MECHANISM_TYPE, DYC_SelfSignX509, DYC_SignX509
 from .defines import CKR_OK
 
 
@@ -20,15 +21,15 @@ def dyc_self_sign_x509(h_session, h_key, hash_alg, subject, serial=None, days=36
     :param int h_key: The key handle to sign with
     :param int hash_alg: Hashing algorithm mechanism type
     :param string subject: Certificate subject string
-    :param string serial: Certificate serial number
-    :returns: (Retcode, Python bytestring of encrypted data)
+    :param bytes serial: Certificate serial number
+    :returns: (Retcode, Python bytestring of self signed X509 certificate)
     :rtype: tuple
 
     Call example:
     ret, x509 = dyc_self_sign_x509(session, priv_key, CKM_SHA256, 'CN=some guy, L=around, C=US')
     """
 
-    c_subj, _ = to_byte_array(from_bytestring(subject))
+    c_subj, _ = to_byte_array(from_bytestring(subject + '\0'))
     c_subj = cast(c_subj, POINTER(c_ubyte))
     serial_len = len(serial) if serial != None else 0
 
@@ -41,6 +42,40 @@ def dyc_self_sign_x509(h_session, h_key, hash_alg, subject, serial=None, days=36
                                 CK_ULONG(days),
                                 x509.array, x509.size)
     ret = _self_sign_x509()
+    if ret != CKR_OK:
+        return ret, None
+
+    x509_pystr = string_at(x509.array, x509.size.contents.value)
+    return ret, x509_pystr
+
+
+def dyc_sign_x509(h_session, h_key, x509CA, hash_alg, csr, serial=None, days=365):
+    """Wrapper for Unbound X509 Self Sign function
+
+    :param int h_session: Current session
+    :param int h_key: The key handle to sign with
+    :param int hash_alg: Hashing algorithm mechanism type
+    :param bytes x509CA: CA Certificate
+    :param bytes csr: Certificate Sign Request
+    :param bytes serial: Certificate serial number
+    :returns: (Retcode, Python bytestring of signed X509 certificate)
+    :rtype: tuple
+
+    """
+
+    serial_len = len(serial) if serial != None else 0
+
+    x509 = AutoCArray(ctype=c_ubyte)
+    @refresh_c_arrays(1)
+    def _sign_x509():
+        return DYC_SignX509(h_session, h_key,
+                            cast(x509CA, POINTER(c_ubyte)), CK_ULONG(len(x509CA)),
+                            CK_MECHANISM_TYPE(hash_alg),
+                            cast(csr, POINTER(c_ubyte)), CK_ULONG(len(csr)),
+                            serial, CK_ULONG(serial_len),
+                            CK_ULONG(days),
+                            x509.array, x509.size)
+    ret = _sign_x509()
     if ret != CKR_OK:
         return ret, None
 
